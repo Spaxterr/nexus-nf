0733609040c32a2e331a59e5e92a24422bf9588e
Spaxterr <spaxtersurvival@gmail.com>
2025-07-06 08:34:36 +0200

    chore: format and refactor configuration files

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,61 +1,30 @@
 module.exports = {
-  // Line width
-  printWidth: 100,
-
-  // Indentation
-  tabWidth: 2,
+  printWidth: 120,
+  tabWidth: 4,
   useTabs: false,
-
-  // Semicolons
   semi: true,
-
-  // Quotes
   singleQuote: true,
   quoteProps: 'as-needed',
-
-  // JSX
   jsxSingleQuote: true,
-
-  // Trailing commas
   trailingComma: 'es5',
-
-  // Brackets
   bracketSpacing: true,
   bracketSameLine: false,
-
-  // Arrow functions
   arrowParens: 'avoid',
-
-  // Range formatting
   rangeStart: 0,
   rangeEnd: Infinity,
-
-  // Parser
   requirePragma: false,
   insertPragma: false,
-
-  // Prose wrap
   proseWrap: 'preserve',
-
-  // HTML whitespace
   htmlWhitespaceSensitivity: 'css',
-
-  // End of line
   endOfLine: 'lf',
-
-  // Embedded language formatting
   embeddedLanguageFormatting: 'auto',
-
-  // Single attribute per line
   singleAttributePerLine: false,
-
-  // Override settings for specific file types
   overrides: [
     {
       files: '*.json',
       options: {
         printWidth: 80,
-        tabWidth: 2,
+        tabWidth: 4,
       },
     },
     {
@@ -94,20 +63,8 @@ module.exports = {
     {
       files: 'package.json',
       options: {
-        tabWidth: 2,
+        tabWidth: 4,
         printWidth: 120,
-      },
-    },
-    {
-      files: '*.test.{js,ts}',
-      options: {
-        printWidth: 120, // Longer lines for test descriptions
-      },
-    },
-    {
-      files: '*.spec.{js,ts}',
-      options: {
-        printWidth: 120, // Longer lines for test descriptions
       },
     },
   ],
