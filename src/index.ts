03fb58646e02f0f5c6266621cf39dc8689ab1ec5
Spaxterr <spaxtersurvival@gmail.com>
2025-07-06 21:25:36 +0200

    feat: export types in index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { Controller, Endpoint } from './core/decorators';
 export { NexusApp } from './core/nexus-nf';
+
+export type { ErrorResponse, SuccessResponse } from './core/nexus-nf';
+
+export type { EndpointHandler, ControllerOptions } from './core/decorators';
