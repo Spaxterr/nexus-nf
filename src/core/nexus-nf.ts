c438e6b84bc742dbeba8b09838e8a8895310d0c8
Spaxterr <spaxtersurvival@gmail.com>
2025-07-06 20:08:28 +0200

    refactor: move endpoint handler wrapping to separate method

--- src/core/nexus-nf.ts
+++ src/core/nexus-nf.ts
@@ -1,5 +1,12 @@
-import { Msg, type NatsConnection, NatsError, type Service, ServiceClient, ServiceGroup } from 'nats';
-import { type NexusController } from './decorators';
+import {
+    type Msg,
+    type NatsConnection,
+    type NatsError,
+    type Service,
+    type ServiceClient,
+    type ServiceGroup,
+} from 'nats';
+import { type EndpointHandler, type NexusController } from './decorators';
 
 export class NexusApp {
     public readonly natsConnection: NatsConnection;
@@ -14,24 +21,34 @@ export class NexusApp {
         this.groups = {};
     }
 
-    public addController(controller: NexusController) {
+    public registerController(controller: NexusController) {
         const group = this.groups[controller.group] ?? this.service.addGroup(controller.group);
         this.groups[controller.group] ??= group;
 
         for (const endpoint of controller.endpoints) {
             group.addEndpoint(endpoint.subject!, {
                 handler: async (err: NatsError | null, msg: Msg) => {
-                    if (err !== null) {
-                        msg.respond(JSON.stringify({ err }));
-                        return;
-                    }
-
-                    const data = msg.json<Parameters<typeof endpoint.handler>>();
-                    const result = await endpoint.handler(data);
-                    msg.respond(result);
+                    await this.wrapHandler(endpoint.handler, err, msg);
                 },
                 queue: controller.queue,
             });
         }
+    }
+
+    private async wrapHandler(handler: EndpointHandler, err: NatsError | null, msg: Msg): Promise<void> {
+        if (err !== null) {
+            msg.respond(JSON.stringify(err));
+            return;
+        }
+
+        let data;
+        try {
+            data = msg.json<Parameters<typeof handler>>();
+        } catch (err) {
+            data = msg.data;
+            console.error(err);
+        }
+        const result = await handler(data);
+        msg.respond(result);
     }
 }
