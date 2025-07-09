8d63e204fef79714643439cb04063b310b666698
Spaxterr <spaxtersurvival@gmail.com>
2025-07-09 22:35:48 +0200

    feat: create errors module

---
+++ src/core/errors.ts
@@ -0,0 +1,11 @@
+export class DuplicateControllerError extends Error {
+    constructor(message?: string) {
+        super(message);
+    }
+}
+
+export class InvalidControllerError extends Error {
+    constructor(message?: string) {
+        super(message);
+    }
+}
