b179a179d98558bf282fda9fbe5b8b1f20b5e1a4
Spaxterr <spaxtersurvival@gmail.com>
2025-07-09 22:57:24 +0200

    chore: ignore docs folder in eslint and prettier

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -6,7 +6,7 @@ const prettierConfig = require('eslint-config-prettier');
 module.exports = [
     {
         files: ['**/*.{js,mjs,cjs,ts,tsx}'],
-        ignores: ['eslint.config.js', 'commitlint.config.js', 'dist/**'],
+        ignores: ['eslint.config.js', 'commitlint.config.js', 'dist/**', 'docs/**'],
         languageOptions: {
             parser: typescriptParser,
             parserOptions: {
