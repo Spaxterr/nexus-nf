1f372a982d34837f97e3465363a76a5e59728b58
Spaxterr <spaxtersurvival@gmail.com>
2025-07-27 00:05:05 +0200

    test: use babel for js modules in test files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@
 export default {
     preset: 'ts-jest',
     collectCoverage: true,
+    setupFilesAfterEnv: ['./src/test/setup.ts'],
+    transform: {
+      '^.+\\.(ts|tsx)?$': 'ts-jest',
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+      "node_modules/(?!(ora|chalk|cli-cursor))",
+    ],
     coveragePathIgnorePatterns: [
       '/node_modules/',
       '/docs/',
